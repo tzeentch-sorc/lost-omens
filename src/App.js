2d4529c3e5e9379f850a32d12f1b852a72fc98e9
tzeentch-sorc <37943035+tzeentch-sorc@users.noreply.github.com>
2023-09-30 23:33:01 +0300

    Get chars list, create buttons

--- src/App.js
+++ src/App.js
@@ -1,28 +1,116 @@
 import React, { useState, useEffect } from 'react';
-import bridge from '@vkontakte/vk-bridge';
-import { View, ScreenSpinner, AdaptivityProvider, AppRoot, ConfigProvider, SplitLayout, SplitCol, Panel, Group, Cell } from '@vkontakte/vkui';
+import bridge, { EGetLaunchParamsResponseLanguages } from '@vkontakte/vk-bridge';
+import { View, ScreenSpinner, AdaptivityProvider, AppRoot, ConfigProvider, SplitLayout, SplitCol, Snackbar, Gradient } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
 import Home from './panels/Home';
-import Persik from './panels/Persik';
+import Intro from './panels/Intro.js'
+
+const ROUTES = {
+	HOME: 'home',
+	INTRO: 'intro',
+}
+
+const STORAGE_KEYS = {
+	STATUS: 'status'
+}
 
 const App = () => {
-	const [activePanel, setActivePanel] = useState('home');
+	const [activePanel, setActivePanel] = useState(ROUTES.INTRO);
 	const [fetchedUser, setUser] = useState(null);
 	const [popout, setPopout] = useState(<ScreenSpinner size='large' />);
+	const [userSeenIntro, setSeenIntro] = useState(false);
+	const [snackbar, setSnackbar] = useState(false);
 
 	useEffect(() => {
 		async function fetchData() {
-			const user = await bridge.send('VKWebAppGetUserInfo');
+			const uid = await bridge.send('VKWebAppGetLaunchParams');
+			const user = await bridge
+				.send('VKWebAppCallAPIMethod', {
+					method: 'users.get',
+					params: {
+						user_ids: uid.vk_user_id,
+						v: '5.131',
+						fields: 'screen_name, photo_200',
+						access_token: '3d1cfde53d1cfde53d1cfde5923e09382633d1c3d1cfde55808b77a146aa66ab68e156d'
+					}
+				}).then(resp => { return resp.response[0] });
+			const storageData = await bridge.send('VKWebAppStorageGet', {
+				keys: Object.values(STORAGE_KEYS)
+			})
+			const data = {};
+			storageData.keys.forEach(({ key, value }) => {
+				try {
+					data[key] = value ? JSON.parse(value) : {};
+					switch (key) {
+						case STORAGE_KEYS.STATUS:
+							if (data[key].seenIntro) {
+								setActivePanel(ROUTES.HOME);
+								setSeenIntro(true);
+							}
+							break;
+						default: break;
+					}
+				} catch (err) {
+					setSnackbar(<Snackbar
+						layout='vertical'
+						onClose={() => setSnackbar(null)}
+						duration={900}
+					>
+						Failed to get data from storage.
+					</Snackbar>)
+				}
+			});
 			setUser(user);
 			setPopout(null);
 		}
 		fetchData();
 	}, []);
 
-	const go = e => {
-		setActivePanel(e.currentTarget.dataset.to);
+	const go = panel => {
+		setActivePanel(panel);
 	};
+
+	const viewIntro = async function () {
+		try {
+			await bridge.send('VKWebAppStorageSet', {
+				key: STORAGE_KEYS.STATUS,
+				value: JSON.stringify({ seenIntro: true }) //TODO true
+			})
+			go(ROUTES.HOME)
+		}
+		catch (err) {
+			setSnackbar(<Snackbar
+				layout='vertical'
+				onClose={() => setSnackbar(null)}
+				duration={900}
+			>
+				Failed to get data from storage.
+				{err}
+			</Snackbar>)
+		}
+	}
+
+	//TODO fix bug when it does not get user when going back to init page
+	const initialState = async function () {
+		try {
+			await bridge.send('VKWebAppStorageSet', {
+				key: STORAGE_KEYS.STATUS,
+				value: JSON.stringify({ seenIntro: false }) //TODO true
+			})
+			go(ROUTES.INTRO)
+		}
+		catch (err) {
+			setSnackbar(<Snackbar
+				layout='vertical'
+				onClose={() => setSnackbar(null)}
+				duration={900}
+			>
+				Failed to get data from storage.
+				{err}
+			</Snackbar>)
+		}
+	}
 
 	return (
 		<ConfigProvider>
@@ -30,9 +118,9 @@ const App = () => {
 				<AppRoot>
 					<SplitLayout popout={popout}>
 						<SplitCol>
-							<View activePanel={activePanel}>
-								<Home id='home' fetchedUser={fetchedUser} go={go} />
-								
+							<View activePanel={activePanel} >
+								<Home id={ROUTES.HOME} fetchedUser={fetchedUser} go={initialState} snackbarErr={snackbar} />
+								<Intro id={ROUTES.INTRO} go={viewIntro} snackbarErr={snackbar} fetchedUser={fetchedUser} seenIntro={userSeenIntro} />
 							</View>
 						</SplitCol>
 					</SplitLayout>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import bridge from "@vkontakte/vk-bridge";
 import App from "./App";
 
+//TODO custom styles
+import '@vkontakte/vkui/dist/components.css';
+import '@vkontakte/vkui-tokens/themes/vkCom/cssVars/declarations/onlyVariables.css';
+import '@vkontakte/vkui-tokens/themes/vkComDark/cssVars/declarations/onlyVariablesLocal.css';
+
 // Init VK Mini App
 bridge.send("VKWebAppInit");
 
