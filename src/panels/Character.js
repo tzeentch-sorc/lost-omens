414898a24f274032deaf0ce2d4755994b9031928
tzeentch-sorc <37943035+tzeentch-sorc@users.noreply.github.com>
2023-12-02 01:52:14 +0300

    Add downtime

--- src/panels/Character.js
+++ src/panels/Character.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Panel, SimpleCell, InfoRow, Header, Button, Group, Div, PanelHeaderBack , PanelHeader, ScreenSpinner, SplitLayout, SplitCol } from '@vkontakte/vkui';
+import {
+	Panel, SimpleCell, InfoRow,
+	Header, Button, Group, Div, PanelHeaderBack, PanelHeader,
+	ScreenSpinner, SplitLayout, SplitCol, CardGrid, Card
+} from '@vkontakte/vkui';
+import { Icon28HourglassOutline, Icon36CoinsStacks3Outline, Icon20UserSquareOutline } from '@vkontakte/icons'
 
 const Character = ({ id, go, fetchedUser, charName, url }) => {
 	const [inventory, setInventory] = useState();
 	const [gold, setGold] = useState(0);
+	const [downtime, setDowntime] = useState(0);
 
 	const [popout, setPopout] = useState(<ScreenSpinner size='large' />)
 
@@ -27,25 +33,31 @@ const Character = ({ id, go, fetchedUser, charName, url }) => {
 			})
 			setInventory(data.inventory);
 			setGold(data.gold);
+			setDowntime(data.downtime);
 			setPopout(null);
 		}
 		fetchData();
 	}, []);
 
 	return (
-		<Panel id={id}>
-			<PanelHeader before={<PanelHeaderBack onClick={go}/>}>Lost Omens</PanelHeader>
-			<Group header={<Header mode="primary">{charName}</Header>}>
-				<SplitLayout popout={popout}>
-					<SplitCol>
-					<Group header={<Header mode="secondary">Инвентарь</Header>}>
-						{inventory && inventory.map(e => createRow(e))}
-					</Group>
-					</SplitCol>
-					<SplitCol>
-						<Group header={<Header mode="secondary">Золота: {gold}</Header>}/>
-					</SplitCol>
-				</SplitLayout>
+		<Panel popout={popout}>
+			<PanelHeader>
+				{charName}
+			</PanelHeader>
+			<Group>
+				<CardGrid size='m'>
+					<Card >
+						<Header mode="primary">Золото</Header>
+						<SimpleCell before={<Icon36CoinsStacks3Outline width={24} height={24} />}>{gold}</SimpleCell>
+					</Card>
+					<Card>
+						<Header mode="primary">Даунтайм</Header>
+						<SimpleCell before={<Icon28HourglassOutline width={24} height={24} />}>{downtime}</SimpleCell>
+					</Card>
+				</CardGrid>
+			</Group>
+			<Group header={<Header mode="primary">Инвентарь</Header>}>
+				{inventory && inventory.map(e => createRow(e))}
 			</Group>
 		</Panel>
 	);
