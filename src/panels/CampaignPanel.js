f084825c7b00af8bd719433b88d454c1c4b10235
Anna Bazarova <bazarova@mineframe.ru>
2024-03-23 09:21:21 +0300

    add modal window need routing

--- src/panels/CampaignPanel.js
+++ src/panels/CampaignPanel.js
@@ -5,10 +5,15 @@ import {
 	Button, Group,
 	PanelHeaderBack, ScreenSpinner,
 	SplitCol, SplitLayout,
-	CardGrid, Card, SimpleCell, Badge
+	CardGrid, Card, SimpleCell, Badge, ModalRoot, ModalCard,
+	ButtonGroup
+
 } from '@vkontakte/vkui';
 
-import { Icon28CalendarOutline, Icon28CrownOutline, Icon24UserOutline, Icon24StatisticsOutline, Icon24InfoCircleOutline } from '@vkontakte/icons'
+import {
+	Icon28CalendarOutline, Icon28CrownOutline, Icon24UserOutline,
+	Icon24StatisticsOutline, Icon24InfoCircleOutline
+} from '@vkontakte/icons'
 import { GOOGLE_SCRIPTS_BASE_URL } from '../App.js'
 import { useSearchParams, useRouteNavigator } from '@vkontakte/vk-mini-apps-router';
 
@@ -28,31 +33,96 @@ const CampaignPanel = ({ fetchedUser }) => {
 	const [prio, setPrio] = useState()
 	const [popout, setPopout] = useState(<ScreenSpinner size='large' />)
 
-	function createCard(element) {
-		return (
-			<Group mode="plain">
-				<CardGrid size="l">
-					<Card mode="shadow" size="m">
-						<SimpleCell
-							key={element.name}
-							id={element.name}
-							onClick={() => {
-								if (element.lvl_up) {
+	const [activeModal, setActiveModal] = useState(null);
+	const [modalHistory, setModalHistory] = useState([]);
+
+	const changeActiveModal = (activeModal) => {
+		activeModal = activeModal || null;
+		let localModalHistory = modalHistory ? [...modalHistory] : [];
+
+		if (activeModal === null) {
+			localModalHistory = [];
+		} else if (modalHistory.indexOf(activeModal) !== -1) {
+			localModalHistory = localModalHistory.splice(0, localModalHistory.indexOf(activeModal) + 1);
+		} else {
+			localModalHistory.push(activeModal);
+		}
+
+		setActiveModal(activeModal);
+		setModalHistory(localModalHistory);
+	};
+
+	const modalBack = () => {
+		changeActiveModal(modalHistory[modalHistory.length - 2]);
+	};
+
+	const modal = (
+		<ModalRoot activeModal={activeModal} onClose={modalBack}>
+			<ModalCard
+				id="confirmLvlup"
+				onClose={() => changeActiveModal(null)}
+				icon={<Icon24StatisticsOutline width={56} height={56} />}
+				header="Вашему персонажу необходимо повышение уровня"
+				subheader="Теперь вы можете перейти к форме повышения уровня прямо из нашего приложения"
+				actions={
+					<React.Fragment>
+						<ButtonGroup mode="horizontal" gap="m" stretched>
+							<Button
+								size="l"
+								mode="primary"
+								stretched
+								onClick={()=>window.open('https://docs.google.com/forms/d/e/1FAIpQLSf4rQ2XSS3zMYp8NLPlh1Oj7eqAMCWFbO7iyW6XdY-i-Aa4dA/viewform', "_blank")}
+							>
+								Повысить уровень
+							</Button>
+							<Button
+								size="l"
+								mode="secondary"
+								stretched
+								onClick={() => {
 									//TODO
-								} else {
 									params.set('CharName', element.name);
 									setParams(params);
 									routeNavigator.push('/char', { keepSearchParams: true });
-								}
+								}}
+							>
+								Продолжить
+							</Button>
+						</ButtonGroup>
 
-							}} before={<Icon24UserOutline width={24} height={24} />} badgeAfterTitle={element.lvl_up && <Badge></Badge>}> {element.name}</SimpleCell>
+					</React.Fragment>
+				}
+			/>
+		</ModalRoot>
+	);
 
-						<SimpleCell
-							key={element.name}
-							id={element.name} before={<Icon24InfoCircleOutline width={24} height={24} />}> {element.race}-{element.type} {element.lvl} уровня</SimpleCell>
-					</Card>
-				</CardGrid>
-			</Group>
+	function createCard(element) {
+		return (
+			<SplitLayout modal={modal}>
+				<SplitCol>
+					<Group mode="plain">
+						<CardGrid size="l">
+							<Card mode="shadow" size="m">
+								<SimpleCell
+									key={element.name}
+									id={element.name}
+									onClick={() => {
+										if (element.lvl_up) {
+											changeActiveModal("confirmLvlup");
+										} else {
+											params.set('CharName', element.name);
+											setParams(params);
+											routeNavigator.push('/char', { keepSearchParams: true });
+										}
+
+									}} before={<Icon24UserOutline width={24} height={24} />} badgeAfterTitle={element.lvl_up && <Badge></Badge>}> {element.name}</SimpleCell>
+
+								<SimpleCell before={<Icon24InfoCircleOutline width={24} height={24} />}> {element.race}-{element.type} {element.lvl} уровня</SimpleCell>
+							</Card>
+						</CardGrid>
+					</Group>
+				</SplitCol>
+			</SplitLayout>
 		);
 	}
 
