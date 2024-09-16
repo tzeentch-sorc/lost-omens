2b75ed4f2eb5ebfa5c0919ce7a8d1d0dc1489387
tzeentch-sorc <37943035+tzeentch-sorc@users.noreply.github.com>
2024-09-17 00:15:24 +0300

    #56 Add money for ouroboros campaign (+updated libs)

--- src/panels/ouroboros/SFCharacter.js
+++ src/panels/ouroboros/SFCharacter.js
@@ -3,9 +3,13 @@ import axios from 'axios';
 import {
 	Panel, SimpleCell,
 	Header, Group, PanelHeaderBack, PanelHeader,
-	ScreenSpinner, CardGrid, Card, Div, SplitCol, SplitLayout
+	ScreenSpinner, CardGrid, Card, Div, SplitCol, SplitLayout,
+	HorizontalCell, Flex
 } from '@vkontakte/vkui';
-import { Icon56Stars3Outline, Icon28TouchIdOutline } from '@vkontakte/icons'
+import {
+	Icon56Stars3Outline, Icon28TouchIdOutline, Icon36CoinsStacks3Outline,
+	Icon24PlaneOutline, Icon28WrenchOutline
+} from '@vkontakte/icons'
 import { SF_GOOGLE_SCRIPTS_BASE_URL } from './SFCampaignPanel.js'
 import { useSearchParams, useRouteNavigator } from '@vkontakte/vk-mini-apps-router';
 
@@ -19,6 +23,10 @@ const SFCharacter = () => {
 	const [exp, setExp] = useState(-1);
 	const [desc, setDesc] = useState("Нет данных");
 	const [hist, setHist] = useState("Нет данных");
+	const [gold, setGold] = useState("Нет данных");
+	const [ship, setShip] = useState("Нет данных");
+	const [mech, setMech] = useState("Нет данных");
+
 
 	const [popout, setPopout] = useState(<ScreenSpinner size='large' />)
 	const charName = params.get('CharName');
@@ -26,7 +34,7 @@ const SFCharacter = () => {
 	useEffect(() => {
 		async function fetchData() {
 			isFinished = false
-			const data = await axios.post(SF_GOOGLE_SCRIPTS_BASE_URL + "?id=" + charName).then(resp => {
+			const data = await axios.post(SF_GOOGLE_SCRIPTS_BASE_URL + "?id=" + charName).then(resp => { //charName  "Кима"
 				return resp.data
 			})
 			setCharId(data.id)
@@ -36,8 +44,15 @@ const SFCharacter = () => {
 				setHist(data.hist)
 			if (data.desc)
 				setDesc(data.desc)
+			if (data.gold)
+				setGold(data.gold)
+			if (data.psVal)
+				setShip(data.psVal)
+			if (data.pmVal)
+				setMech(data.pmVal)
 			setPopout(<ScreenSpinner state="done">Успешно</ScreenSpinner>);
 			setTimeout(() => setPopout(null), 1000);
+			console.log(data)
 		}
 		fetchData();
 	}, []);
@@ -60,6 +75,28 @@ const SFCharacter = () => {
 								<Card key="level">
 									<Header mode="primary">Уровень и опыт</Header>
 									<SimpleCell before={<Icon56Stars3Outline width={24} height={24} />}>{lvl} ({exp} XP)</SimpleCell>
+								</Card>
+							</CardGrid>
+							<CardGrid size="l" >
+								<Card key="gold">
+									<Header mode="primary">Финансы</Header>
+									<Flex noWrap justify='center'>
+										<HorizontalCell size='l' >
+											<SimpleCell subhead='Кредиты' before={<Icon36CoinsStacks3Outline width={24} height={24} />}>
+												{gold}
+											</SimpleCell>
+										</HorizontalCell>
+										<HorizontalCell size='l' >
+											<SimpleCell subhead='Корабль' before={<Icon24PlaneOutline width={24} height={24} />}>
+												{ship}
+											</SimpleCell>
+										</HorizontalCell>
+										<HorizontalCell size='l' >
+											<SimpleCell subhead='Мех' before={<Icon28WrenchOutline width={24} height={24} />}>
+												{mech}
+											</SimpleCell>
+										</HorizontalCell>
+									</Flex>
 								</Card>
 							</CardGrid>
 							<CardGrid size="l" >
