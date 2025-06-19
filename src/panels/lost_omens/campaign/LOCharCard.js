51e1d5c5a5312cfcf027b558b4bedd97693c5730
Anna Bazarova <bazarova@mineframe.ru>
2025-06-19 21:05:41 +0300

    Placeholder icons for LO

--- src/panels/lost_omens/campaign/LOCharCard.js
+++ src/panels/lost_omens/campaign/LOCharCard.js
@@ -1,11 +1,59 @@
 import { Card, RichCell, Counter, Image } from "@vkontakte/vkui";
 import { Icon28UserOutgoingOutline, Icon24UserOutline } from "@vkontakte/icons";
 
-import Icon48Warlock from "../../common/custom_icons/Icon48Warlock.tsx";
+import Icon48Alchemist from "../../common/custom_icons/Icon48Alchemist.tsx";
+import Icon48Animist from "../../common/custom_icons/Icon48Animist.tsx";
+import Icon48Barbarian from "../../common/custom_icons/Icon48Barbarian.tsx";
+import Icon48Bard from "../../common/custom_icons/Icon48Bard.tsx";
+import Icon48Cleric from "../../common/custom_icons/Icon48Cleric.tsx";
+import Icon48Druid from "../../common/custom_icons/Icon48Druid.tsx";
+import Icon48Fighter from "../../common/custom_icons/Icon48Fighter.tsx";
+import Icon48Gunslinger from "../../common/custom_icons/Icon48Gunslinger.tsx";
+import Icon48Inventor from "../../common/custom_icons/Icon48Inventor.tsx";
+import Icon48Investigator from "../../common/custom_icons/Icon48Investigator.tsx";
+import Icon48Kineticist from "../../common/custom_icons/Icon48Kineticist.tsx";
+import Icon48Magus from "../../common/custom_icons/Icon48Magus.tsx";
+import Icon48Monk from "../../common/custom_icons/Icon48Monk.tsx";
+import Icon48Oracle from "../../common/custom_icons/Icon48Oracle.tsx";
+import Icon48Paladin from "../../common/custom_icons/Icon48Paladin.tsx";
+import Icon48Psychic from "../../common/custom_icons/Icon48Psychic.tsx";
+import Icon48Ranger from "../../common/custom_icons/Icon48Ranger.tsx";
+import Icon48Rogue from "../../common/custom_icons/Icon48Rogue.tsx";
+import Icon48Sorcerer from "../../common/custom_icons/Icon48Sorcerer.tsx";
+import Icon48Summoner from "../../common/custom_icons/Icon48Summoner.tsx";
+import Icon48Swashbuckler from "../../common/custom_icons/Icon48Swashbuckler.tsx";
+import Icon48Taumaturg from "../../common/custom_icons/Icon48Taumaturg.tsx";
+import Icon48Witch from "../../common/custom_icons/Icon48Witch.tsx";
+import Icon48Wizard from "../../common/custom_icons/Icon48Wizard.tsx";
+
 
 const LOCharCard = ({element, openAction}) => {
     const iconMap = {
-        "Колдун": <Icon48Warlock color='#008cff'/>,
+        "Алхимик": <Icon48Alchemist color='#008cff'/>,
+        "Анимист": <Icon48Animist color='#008cff'/>,
+        "Варвар": <Icon48Barbarian color='#008cff'/>,
+        "Бард": <Icon48Bard color='#008cff'/>,
+        "Жрец": <Icon48Cleric color='#008cff'/>,
+        "Друид": <Icon48Druid color='#008cff'/>,
+        "Воин": <Icon48Fighter color='#008cff'/>,
+        "Стрелок": <Icon48Gunslinger color='#008cff'/>,
+        "Изобретатель": <Icon48Inventor color='#008cff'/>,
+        "Сыщик": <Icon48Investigator color='#008cff'/>,
+        "Кинетик": <Icon48Kineticist color='#008cff'/>,
+        "Магус": <Icon48Magus color='#008cff'/>,
+        "Монах": <Icon48Monk color='#008cff'/>,
+        "Оракул": <Icon48Oracle color='#008cff'/>,
+        "Поборник": <Icon48Paladin color='#008cff'/>,
+        "Психик": <Icon48Psychic color='#008cff'/>,
+        "Следопыт": <Icon48Ranger color='#008cff'/>,
+        "Плут": <Icon48Rogue color='#008cff'/>,
+        "Чародей": <Icon48Sorcerer color='#008cff'/>,
+        "Призыватель": <Icon48Summoner color='#008cff'/>,
+        "Сорвиголова": <Icon48Swashbuckler color='#008cff'/>,
+        "Тавматург": <Icon48Taumaturg color='#008cff'/>,
+        "Ведьма": <Icon48Witch color='#008cff'/>,
+        "Волшебник": <Icon48Wizard color='#008cff'/>,
+
         // Add more types and icons as needed
     };
 
