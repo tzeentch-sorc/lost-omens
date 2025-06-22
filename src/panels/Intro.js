fab8405203ea72c8d6e6d56fed54069107b5f166
BazarovaAnna <Anna.Bazarova.99@gmail.com>
2025-06-22 13:02:37 +0300

    fix

--- src/panels/Intro.js
+++ src/panels/Intro.js
@@ -4,7 +4,7 @@ import { useSearchParams, useRouteNavigator } from '@vkontakte/vk-mini-apps-rout
 
 
 import './Intro.css'
-import CampaignCard from "./common/CampaignCard";
+import CampaignCard from "./common/components/CampaignCard";
 
 import {LOCampaign, SMCampaign, SFCampaign, BWCampaign, VUCampaign, RGCampaign, HGCampaign} from '../util/consts.js';
 
