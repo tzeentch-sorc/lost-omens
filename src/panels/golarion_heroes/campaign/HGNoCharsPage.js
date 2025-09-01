a8762531d7df3543c4901d36e60c6a8b2288f683
Anna Bazarova <bazarova@mineframe.ru>
2025-09-01 11:41:39 +0300

    Borrowed files from 55-add-pf1

---
+++ src/panels/golarion_heroes/campaign/HGNoCharsPage.js
@@ -0,0 +1,85 @@
+import {
+    Panel, PanelHeader, Header,
+    Button, Group,
+    PanelHeaderBack,
+    SplitCol, SplitLayout,
+    Div, Placeholder,
+    ScreenSpinner
+
+} from '@vkontakte/vkui';
+import {
+    Icon56UserAddOutline
+} from '@vkontakte/icons'
+import { useRouteNavigator } from '@vkontakte/vk-mini-apps-router';
+import React, { useEffect, useState } from 'react';
+import bridge from '@vkontakte/vk-bridge';
+import MastersGroup from '../../common/components/MastersGroup.js';
+import ArticleBlock from '../../common/components/ArticleBlock.js';
+import HGMastersInfoSettings from '../export_settings/HGMastersInfoSettings.js'
+
+
+const HGNoCharsPage = ({ campaignName, user, }) => {
+
+    const [popout, setPopout] = useState(<ScreenSpinner size='large' />)
+    const [isDisplayed, setIsDisplayed] = useState(false);
+    const [masters, setMasters] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const masterData = await HGMastersInfoSettings.getQueryAll();
+			const userIds = masterData.map(elem => elem.id).join(', ');
+            const users = await bridge
+                .send('VKWebAppCallAPIMethod', {
+                    method: 'users.get',
+                    params: {
+                        user_ids: userIds,
+                        v: '5.131',
+                        fields: 'screen_name, photo_200',
+                        access_token: '3d1cfde53d1cfde53d1cfde5923e09382633d1c3d1cfde55808b77a146aa66ab68e156d'
+                    }
+                }).then(resp => { return resp.response });
+
+            setMasters(users);
+            setPopout(<ScreenSpinner state="done">Успешно</ScreenSpinner>);
+            setTimeout(() => { setPopout(null); setIsDisplayed(true); }, 1000)
+        }
+        fetchData().catch(console.error);
+    }, []);
+
+    const routeNavigator = useRouteNavigator();
+
+    return (
+        <Panel nav='campaign' key={campaignName}>
+            <PanelHeader before={<PanelHeaderBack onClick={() => routeNavigator.replace('/')} />}>{campaignName}</PanelHeader>
+            {
+                user && masters &&
+                <Group mode='plain'>
+                    <SplitLayout popout={popout}>
+                        {isDisplayed &&
+                            <SplitCol>
+                                <Group mode="plain">
+                                    <ArticleBlock
+                                        articleLink='https://vk.com/@geekmo-pathfinder-campaign'
+                                        caption='Добро пожаловать к «Героям Голариона»!'
+                                        description='Введение в мир Голариона 1й редакции'
+                                        image='/images/hg_banner_article.png'                                    
+                                    />
+                                    <MastersGroup masters={masters} />
+                                    <Group mode="card">
+                                        <Placeholder icon={<Icon56UserAddOutline />} header="Создание персонажа">
+                                            <Div>
+                                                Для создания стоит написать одному из мастеров
+                                            </Div>
+                                        </Placeholder>
+                                    </Group>
+                                </Group>
+                            </SplitCol>
+                        }
+                    </SplitLayout>
+                </Group>
+            }
+        </Panel>
+    );
+}
+
+export default HGNoCharsPage;
---
+++ src/panels/golarion_heroes/export_settings/HGMastersInfoSettings.js
@@ -0,0 +1,28 @@
+import QuerySettings from '../../../util/QuerySettings';
+
+const sheetId = "1FRkKmWu44biVjYP7cZJkG5RkxkwnjaSPVTke3tsZwp0"; // HG Geekmo Mirror
+
+const HGMastersInfoSettings = new QuerySettings({
+	sheetId,
+	gid: 111508886, //sheet "masters"
+	headrow: 1,
+	fields: {
+		id: "VK ID",
+		name: "Мастер"	
+	},
+	query: {
+		colByField: { 
+			id: 0,
+			name: 1
+		 },
+		fieldByCol: { 
+			0: "id",
+			1: "name"
+		 },
+		colIDs: [ 0, 1 ],
+		queryAll: "select A, B",
+	},
+	range: { min: 0, max: 1, str: "A1:B" },
+});
+
+export default HGMastersInfoSettings;
