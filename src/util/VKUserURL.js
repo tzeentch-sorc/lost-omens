de0113f3666f8f9c5e90bec874ab398c423b8eea
Anna Bazarova <bazarova@mineframe.ru>
2025-09-02 12:33:52 +0300

    moving stuff around

--- src/util/VKUserURL.js
+++ src/util/VKUserURL.js
@@ -3,7 +3,7 @@ export const getVkUserUrl = (elem, fetchedUser) => {
     if (process.env.NODE_ENV === 'development') {
         // Running with npm start
         //DEBUG:
-        return elem.id == ("vk.com/_magic");
+        return elem.id == ("vk.com/emmalain");
     } else {
         // Running with npm run deploy (production)
         return elem.id == ("vk.com/" + fetchedUser.screen_name) || elem.id == ("vk.com/id" + fetchedUser.id);
