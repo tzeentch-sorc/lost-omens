cfe0c9e898c1ac181e74fd965e81eb7b47f00200
BazarovaAnna <Anna.Bazarova.99@gmail.com>
2025-12-04 23:32:03 +0300

    hotfix

--- src/panels/form/Form.js
+++ src/panels/form/Form.js
@@ -14,6 +14,7 @@ import InfoKT from "./InfoKT.js";
 import InfoBT from "./InfoBT.js";
 import InfoSeparateEvent from "./InfoSeparateEvent.js";
 import InfoFinish from "./InfoFinish.js";
+import InfoSent from "./InfoSent.js";
 import FormCredentials from './FormCredentials.js';
 import FormItmo from "./FormItmo.js";
 import FormNotItmo from "./FormNotItmo.js";
